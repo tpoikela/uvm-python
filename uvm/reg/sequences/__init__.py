ba490ecfac91c70ccb91ef37611cd14ab020ecab
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2019-12-15 20:13:56 +0200

    Added _init__.py

---
+++ uvm/reg/sequences/__init__.py
@@ -0,0 +1,9 @@
+
+from .uvm_mem_access_seq import *
+from .uvm_mem_walk_seq import *
+from .uvm_reg_access_seq import *
+from .uvm_reg_bit_bash_seq import *
+from .uvm_reg_hw_reset_seq import *
+from .uvm_reg_mem_built_in_seq import *
+from .uvm_reg_mem_hdl_paths_seq import *
+from .uvm_reg_mem_shared_access_seq import *
