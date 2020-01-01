a04a7a9c6a833f1c880a6dc769721f2543bef64c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-01-01 20:29:26 +0200

    Added more features to sv2py.pl

--- bin/sv2py.pl
+++ bin/sv2py.pl
@@ -27,63 +27,100 @@ use Getopt::Long;
 
 my %opt;
 GetOptions(
-    "file|f=s" => \$opt{f}
+    "file|f=s" => \$opt{f},
+    "all" => \$opt{all}
 );
 
-open(my $IFILE, "<", $opt{f}) or die $!;
+my @files = ();
 
-my @outfile = ();
-
-my $ind = 0;
-
-my $def_conn_phase = "def connect_phase(self, phase):";
-my $def_build_phase = "def build_phase(self, phase):";
-
-while (<$IFILE>) {
-    my $line = $_;
-    my $ws = " " x (4 * $ind);
-
-    if ($line =~ /^(.*)All rights reserved worldwide/i) {
-        my $cline = "#$1Copyright 2019-2020 Tuomas Poikela (tpoikela)\n";
-        push(@outfile, $cline);
-    }
-
-    if ($line =~ /^\s*(virtual\s+)?task /) {
-        push(@outfile, "$ws#\@cocotb.coroutine\n");
-    }
-    
-    $line =~ s/;$//g;  # Remove trailing semicolon
-    $line =~ s/\s*==\s*null/ is None/g;
-    $line =~ s/\s*!=\s*null/ is not None/g;
-    $line =~ s/\bnull\b/None/g;
-    $line =~ s/\bthis\b/self/g;
-    $line =~ s/\) begin\b/):/g;
-    $line =~ s/\belse\s+if\b/elif/g;
-    $line =~ s/function new\(string name/def __init__(self, name/g;
-    $line =~ s/::type_id::create/.type_id.create/g;
-    $line =~ s/super\.new\(/super().__init__(/g;
-
-
-    # Replace standard UVM functions
-    $line =~ s/virtual function void build\(\)/$def_build_phase/g;
-    $line =~ s/virtual function void connect\(\)/$def_build_phase/g;
-    $line =~ s/virtual function void connect_phase\(uvm_phase phase\)/$def_conn_phase/g;
-    $line =~ s/virtual function void build_phase\(uvm_phase phase\)/$def_build_phase/g;
-    $line =~ s/class (\w+) extends (\w+)/class $1($2):/g;
-
-    # SystemVerilog functions
-    $line =~ s/\$(urandom|sformatf|cast)/sv.$1/g;
-    
-
-    $line =~ s/uvm_config_db#\(.*\)::(set|get)/UVMConfigDb.$1/g;
-    $line =~ s/uvm_resource_db#\(.*\)::(set|get)/UVMResourceDb.$1/g;
-
-    push(@outfile, "$ws#$line");
-
-    if ($line =~ /^class /) {++$ind;}
-    elsif ($line =~ /^endclass/) {--$ind;}
+if (-e $opt{f}) {
+    process_file($opt{f});
+}
+elsif (defined $opt{all}) {
+    @files = glob("*.sv");
+    push(@files, glob("*.svh"));
+}
+else {
+    @files = @ARGV;
 }
 
-for my $line (@outfile) {
-    print $line;
+for my $file (@files) {
+    my $res = process_file($file);
+    if ($file =~ /(\w+)\.svh?/) {
+        my $py_file = "$1.py";
+        open(my $OFILE, ">", $py_file) or die $!;
+        print $OFILE $res;
+        close($OFILE);
+        print "Created new file $py_file\n";
+    }
+}
+
+#---------------------------------------------------------------------------
+# HELPERS
+#---------------------------------------------------------------------------
+
+sub process_file {
+    my ($fname) = @_;
+    open(my $IFILE, "<", $fname) or die $!;
+
+    my @outfile = ();
+    my $ind = 0;
+    my $def_conn_phase = "def connect_phase(self, phase):";
+    my $def_build_phase = "def build_phase(self, phase):";
+
+    my $import_line = -1;
+
+    while (<$IFILE>) {
+        my $line = $_;
+        my $ws = " " x (4 * $ind);
+
+        if ($line =~ /^(.*)All rights reserved worldwide/i) {
+            my $cline = "#$1Copyright 2019-2020 Tuomas Poikela (tpoikela)\n";
+            push(@outfile, $cline);
+        }
+
+        if ($line =~ /^\s*(virtual\s+)?task /) {
+            push(@outfile, "$ws#\@cocotb.coroutine\n");
+        }
+        
+        $line =~ s/;$//g;  # Remove trailing semicolon
+        $line =~ s/\s*==\s*null/ is None/g;
+        $line =~ s/\s*!=\s*null/ is not None/g;
+        $line =~ s/\bnull\b/None/g;
+        $line =~ s/\bthis\b/self/g;
+        $line =~ s/\) begin\b/):/g;
+        $line =~ s/\belse\s+if\b/elif/g;
+        $line =~ s/function new\(string name/def __init__(self, name/g;
+        $line =~ s/::type_id::create/.type_id.create/g;
+        $line =~ s/super\.new\(/super().__init__(/g;
+
+
+        # Replace standard UVM functions
+        $line =~ s/virtual function void build\(\)/$def_build_phase/g;
+        $line =~ s/virtual function void connect\(\)/$def_build_phase/g;
+        $line =~ s/virtual function void connect_phase\(uvm_phase phase\)/$def_conn_phase/g;
+        $line =~ s/virtual function void build_phase\(uvm_phase phase\)/$def_build_phase/g;
+        $line =~ s/class (\w+) extends (\w+)/class $1($2):/g;
+
+        # SystemVerilog functions
+        $line =~ s/\$(urandom|sformatf|cast)/sv.$1/g;
+        
+
+        $line =~ s/uvm_config_db#\(.*\)::(set|get)/UVMConfigDb.$1/g;
+        $line =~ s/uvm_resource_db#\(.*\)::(set|get)/UVMResourceDb.$1/g;
+
+        push(@outfile, "$ws#$line");
+
+        if ($line =~ /^class /) {++$ind;}
+        elsif ($line =~ /^endclass/) {--$ind;}
+    }
+
+    if (defined $opt{f}) {
+        for my $line (@outfile) {
+            print $line;
+        }
+    }
+
+    close($IFILE);
+    return wantarray ? @outfile : join('', @outfile);
 }
