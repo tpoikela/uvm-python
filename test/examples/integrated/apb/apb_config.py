92c5a676c6be23a8fd482f718ac81000afd4d916
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-01-04 23:19:08 +0200

    Added APB config

---
+++ test/examples/integrated/apb/apb_config.py
@@ -0,0 +1,35 @@
+#//
+#// -------------------------------------------------------------
+#//    Copyright 2004-2011 Synopsys, Inc.
+#//    Copyright 2010 Mentor Graphics Corporation
+#//    Copyright 2019-2020 Tuomas Poikela (tpoikela)
+#//    All Rights Reserved Worldwide
+#//
+#//    Licensed under the Apache License, Version 2.0 (the
+#//    "License"); you may not use self file except in
+#//    compliance with the License.  You may obtain a copy of
+#//    the License at
+#//
+#//        http://www.apache.org/licenses/LICENSE-2.0
+#//
+#//    Unless required by applicable law or agreed to in
+#//    writing, software distributed under the License is
+#//    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#//    CONDITIONS OF ANY KIND, either express or implied.  See
+#//    the License for the specific language governing
+#//    permissions and limitations under the License.
+#// -------------------------------------------------------------
+
+from uvm.base.uvm_object import *
+from uvm.macros import *
+
+
+class apb_config(UVMObject):
+
+
+    def __init__(self, name="apb_config"):
+        super().__init__(name)
+        self.vif = None  # apb_if
+
+
+uvm_object_utils(apb_config)
