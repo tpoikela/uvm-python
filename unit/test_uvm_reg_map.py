3bfb47832e4ab7c068a1f44d90d9ed81fc95c21f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-01-21 23:19:41 +0200

    Added test_add_parent_map stub

--- unit/test_uvm_reg_map.py
+++ unit/test_uvm_reg_map.py
@@ -11,9 +11,14 @@ class TestUVMRegMap(unittest.TestCase):
         reg_blk = UVMRegBlock('my_blk')
         reg_map.configure(reg_blk, base_addr=0, n_bytes=32, endian=0,
                 byte_addressing=1)
+        reg_blk.add_map(reg_map)
         r1 = UVMReg('r1', 32, False)
         r1.configure(reg_blk)
         reg_map.add_reg(r1, 4)
+        reg_blk.lock_model()
         self.assertEqual(r1.get_offset(), 4)
         r1.set_offset(reg_map, 16)
         self.assertEqual(r1.get_offset(), 16)
+
+    def test_add_parent_map(self):
+        pass
