e73c09fb4169343d1a9e30e18cd8c99c1d99bdfb
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-01-24 00:11:33 +0200

    Added input clk, rst for verilator

--- test/examples/simple/common_stub.v
+++ test/examples/simple/common_stub.v
@@ -1,6 +1,15 @@
 
 `timescale 1ns/1ps
 
-module common_stub();
+module common_stub(
+    input clk, input rst
+);
+
+reg[7:0] word;
 /* Does nothing */
+
+always_ff@(posedge clk or negedge rst)
+    if (!rst) word <= 8'h00;
+    else word <= word + 8'h01;
+
 endmodule
