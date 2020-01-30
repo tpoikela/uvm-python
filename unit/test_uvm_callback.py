bf9d2c2ef5f3ef91dcc8031f95a621cb72a6dded
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-01-30 21:11:59 +0200

    Added test for registering callbacks

--- unit/test_uvm_callback.py
+++ unit/test_uvm_callback.py
@@ -2,9 +2,14 @@
 import unittest
 
 from uvm.base.uvm_callback import (UVMCallback, UVMCallbacks,
-    UVMCallbackIter, UVMTypedCallbacks)
+    UVMCallbackIter, UVMTypedCallbacks, UVMCallbacksBase)
+
+from uvm.base.uvm_component import UVMComponent
 
 from uvm.base.uvm_unit import MockObj, MockCb
+from uvm.macros import (uvm_register_cb, uvm_do_callbacks)
+
+UVMCallbacksBase.m_tracing = True
 
 
 class TestUVMCallback(unittest.TestCase):
@@ -29,19 +34,55 @@ class TestUVMCallback(unittest.TestCase):
         my_iter = UVMCallbackIter(obj)
         cb = my_iter.first()
         self.assertEqual(cb, None)
+        cb = my_iter.last()
+        self.assertEqual(cb, None)
+        cb_obj = MockCb("cb_obj")
+        UVMCallbacks.add(obj, cb_obj)
+        cb = my_iter.first()
+        self.assertEqual(cb, cb_obj)
+        cb = my_iter.last()
+        self.assertEqual(cb, cb_obj)
 
     def test_uvm_callbacks(self):
         parent_obj = MockObj("parent", None)
         cb_obj = MockCb("cb_obj")
         my_iter = UVMCallbackIter(parent_obj)
         UVMCallbacks.add(parent_obj, cb_obj)
-        # cbs = UVMCallbacks.get()
-        #self.assertEqual(cbs.m_inst.m_typeid.typename, 'UVMCallbacks')
+
         cb = UVMCallbacks.get_first(my_iter, parent_obj)
         self.assertEqual(cb, cb_obj)
 
         cb2 = my_iter.first()
         self.assertEqual(cb2, cb_obj)
+
+        class MyCallback(UVMCallback):
+            def get_name(self):
+                return "my_cb"
+            def do_it(self):
+                self.called = True
+            def get_type_name(self):
+                return "MyCallback"
+
+        class MyCallbackWrong(UVMCallback):
+            def do_it(self):
+                raise Exception('Dont call this')
+            def get_type_name(self):
+                return "MyCallbackWrong"
+
+        class MyClass(UVMComponent):
+            def do_it(self):
+                uvm_do_callbacks(self, MyCallback, 'do_it')
+        uvm_register_cb(MyClass, MyCallback)
+        uvm_register_cb(MyClass, MyCallbackWrong)
+
+        my_class = MyClass('my_class_obj', None)
+        my_cb = MyCallback()
+        UVMCallbacks.add(my_class, my_cb)
+        UVMCallbacks.add(my_class, MyCallbackWrong())
+        my_class.do_it()
+        self.assertTrue(my_cb.called)
+
+
 
 
 if __name__ == '__main__':
