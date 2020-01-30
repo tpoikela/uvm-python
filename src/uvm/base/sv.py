777d9fb0bd94f17524dfedd6d57372d6705b5250
Matthew Ballance <matt.ballance@gmail.com>
2020-01-29 20:42:53 -0500

    remove extraneous import from sv module
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/uvm/base/sv.py
+++ src/uvm/base/sv.py
@@ -16,7 +16,6 @@
 #//   the License for the specific language governing
 #//   permissions and limitations under the License.
 #//----------------------------------------------------------------------
-import traceback
 
 """ Some SystemVerilog system functions mocked here """
 
