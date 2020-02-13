01e47821993e73e4c7bce7c52627c0b999fcec97
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-02-13 23:10:01 +0200

    Added first checks for ResourceDbClassFactory

--- unit/test_uvm_resource_db.py
+++ unit/test_uvm_resource_db.py
@@ -9,5 +9,14 @@ class TestUVMResourceDb(unittest.TestCase):
 
     def test_create_class(self):
         DbInt = ResourceDbClassFactory('DbInt', ['_id', 'val2'], int)
-        my_db = DbInt(_id=123, val2='name')
-        self.assertEqual(my_db.T, int)
+        DbStr = ResourceDbClassFactory('DbStr', ['_id', 'val2'], str)
+        my_db_int = DbInt(_id=123, val2='name')
+        my_db_str = DbStr(_id=123, val2='name')
+        self.assertEqual(my_db_int.T, int)
+        self.assertEqual(my_db_str.T, str)
+        my_db_int.set('*', 'value', 123)
+        my_db_str.set('*', 'value', 'str_value')
+        rsc_int = my_db_int.get_by_name('*', 'value')
+        rsc_str = my_db_str.get_by_name('*', 'value')
+        self.assertEqual(rsc_int.read(), 123)
+        # TODO self.assertEqual(rsc_str.read(), 'str_value')
