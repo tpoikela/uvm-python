83419326c1b0a5eb44454747aac5c1c4360be5e5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-02-14 23:14:27 +0200

    Implemented bit-bash seqs

--- src/uvm/reg/sequences/uvm_reg_bit_bash_seq.py
+++ src/uvm/reg/sequences/uvm_reg_bit_bash_seq.py
@@ -25,7 +25,7 @@ import cocotb
 from cocotb.triggers import Timer
 
 from uvm.reg.uvm_reg_sequence import UVMRegSequence
-from uvm.reg.uvm_reg_model import UVM_NO_HIER
+from uvm.reg.uvm_reg_model import (UVM_NO_HIER, UVM_NO_CHECK)
 from uvm.macros import *
 from uvm.base.uvm_resource_db import UVMResourceDb
 
@@ -76,9 +76,9 @@ class UVMRegSingleBitBashSeq(UVMRegSequence):  # (uvm_sequence #(uvm_reg_item))
     @cocotb.coroutine
     def body(self):
         fields = []  # uvm_reg_field[$]
-        mode = []  # string [`UVM_REG_DATA_WIDTH]
+        mode = [""] * UVM_REG_DATA_WIDTH  # string [`UVM_REG_DATA_WIDTH]
         maps = []  # uvm_reg_map [$]
-        dc_mask = 0x0
+        dc_mask = [0] * 32
         reset_val = 0x0
         n_bits = 0
         field_access = ""
@@ -104,93 +104,92 @@ class UVMRegSingleBitBashSeq(UVMRegSequence):  # (uvm_sequence #(uvm_reg_item))
         rg.get_maps(maps)
 
         # TODO Bash the bits in the register via each map
-        #      foreach (maps[j]):
-        #         uvm_status_e status
-        #         uvm_reg_data_t  val, exp, v
-        #         int next_lsb
-        #
-        #         next_lsb = 0
-        #         dc_mask  = 0
-        #         foreach (fields[k]):
-        #            int lsb, w, dc
-        #
-        #            field_access = fields[k].get_access(maps[j])
-        #            dc = (fields[k].get_compare() == UVM_NO_CHECK)
-        #            lsb = fields[k].get_lsb_pos()
-        #            w   = fields[k].get_n_bits()
-        #            // Ignore Write-only fields because
-        #            // you are not supposed to read them
-        #            case (field_access)
-        #             "WO", "WOC", "WOS", "WO1", "NOACCESS": dc = 1
-        #            endcase
-        #            // Any unused bits on the right side of the LSB?
-        #            while (next_lsb < lsb) mode[next_lsb++] = "RO"
-        #
-        #            repeat (w):
-        #               mode[next_lsb] = field_access
-        #               dc_mask[next_lsb] = dc
-        #               next_lsb++
-        #            end
-        #         end
-        #         // Any unused bits on the left side of the MSB?
-        #         while (next_lsb < `UVM_REG_DATA_WIDTH)
-        #            mode[next_lsb++] = "RO"
-        #
-        #         `uvm_info("UVMRegBitBashSeq", sv.sformatf("Verifying bits in register %s in map \"%s\"...",
-        #                                    rg.get_full_name(), maps[j].get_full_name()),UVM_LOW)
-        #
-        #         // Bash the kth bit
-        #         for (int k = 0; k < n_bits; k++):
-        #            // Cannot test unpredictable bit behavior
-        #            if (dc_mask[k]) continue
-        #
-        #            bash_kth_bit(rg, k, mode[k], maps[j], dc_mask)
-        #         end
-        #
-        #      end
+        for j in range(len(maps)):
+            status = 0
+            val = 0x0
+            exp = 0x0
+            v = 0x0
+            next_lsb = 0
+            dc_mask  = [0] * 32
+
+            for k in range(len(fields)):
+                lsb = 0
+                w = 0
+                dc = 0
+
+                field_access = fields[k].get_access(maps[j])
+                dc = (fields[k].get_compare() == UVM_NO_CHECK)
+                lsb = fields[k].get_lsb_pos()
+                w   = fields[k].get_n_bits()
+                # Ignore Write-only fields because
+                # you are not supposed to read them
+                if field_access in ["WO", "WOC", "WOS", "WO1", "NOACCESS"]:
+                    dc = 1
+
+                # Any unused bits on the right side of the LSB?
+                while next_lsb < lsb:
+                    mode[next_lsb] = "RO"
+                    next_lsb += 1
+
+                for _ in range(w):
+                    mode[next_lsb] = field_access
+                    dc_mask[next_lsb] = dc
+                    next_lsb += 1
+            # Any unused bits on the left side of the MSB?
+            while next_lsb < UVM_REG_DATA_WIDTH:
+                mode[next_lsb] = "RO"
+                next_lsb += 1
+
+            uvm_info("UVMRegBitBashSeq", sv.sformatf("Verifying bits in register %s in map \"%s\"...",
+                rg.get_full_name(), maps[j].get_full_name()),UVM_LOW)
+
+            # Bash the kth bit
+            for k in range(n_bits):
+                # Cannot test unpredictable bit behavior
+                if dc_mask[k] == 1:
+                    continue
+                yield self.bash_kth_bit(rg, k, mode[k], maps[j], dc_mask)
+
     #   endtask: body
 
 
-    #@cocotb.coroutine
-    #   def bash_kth_bit(self,uvm_reg         rg,
-    #                     int             k,
-    #                     string          mode,
-    #                     uvm_reg_map     map,
-    #                     uvm_reg_data_t  dc_mask)
-    #      uvm_status_e status
-    #      uvm_reg_data_t  val, exp, v
-    #      bit bit_val
-    #
-    #      `uvm_info("UVMRegBitBashSeq", sv.sformatf("...Bashing %s bit #%0d", mode, k),UVM_HIGH)
-    #
-    #      repeat (2):
-    #         val = rg.get()
-    #         v   = val
-    #         exp = val
-    #         val[k] = ~val[k]
-    #         bit_val = val[k]
-    #
-    #         rg.write(status, val, UVM_FRONTDOOR, map, self)
-    #         if (status != UVM_IS_OK):
-    #            `uvm_error("UVMRegBitBashSeq", sv.sformatf("Status was %s when writing to register \"%s\" through map \"%s\".",
-    #                                        status.name(), rg.get_full_name(), map.get_full_name()))
-    #         end
-    #
-    #         exp = rg.get() & ~dc_mask
-    #         rg.read(status, val, UVM_FRONTDOOR, map, self)
-    #         if (status != UVM_IS_OK):
-    #            `uvm_error("UVMRegBitBashSeq", sv.sformatf("Status was %s when reading register \"%s\" through map \"%s\".",
-    #                                        status.name(), rg.get_full_name(), map.get_full_name()))
-    #         end
-    #
-    #         val &= ~dc_mask
-    #         if (val != exp):
-    #            `uvm_error("UVMRegBitBashSeq", sv.sformatf("Writing a %b in bit #%0d of register \"%s\" with initial value 'h%h yielded 'h%h instead of 'h%h",
-    #                                        bit_val, k, rg.get_full_name(), v, val, exp))
-    #         end
-    #      end
-    #   endtask: bash_kth_bit
-    #
+    @cocotb.coroutine
+    def bash_kth_bit(self, rg, k, mode, map, dc_mask):
+        status = 0
+        val = 0x0
+        exp = 0x0
+        v = 0x0
+        bit_val = False
+
+        uvm_info("UVMRegBitBashSeq", sv.sformatf("...Bashing %s bit #%0d", mode, k),UVM_HIGH)
+
+        for _ in range(2):
+            val = rg.get()
+            v   = val
+            exp = val
+            val[k] = ~val[k]
+            bit_val = val[k]
+
+            yield rg.write(status, val, UVM_FRONTDOOR, map, self)
+            if status != UVM_IS_OK:
+                uvm_error("UVMRegBitBashSeq",
+                        sv.sformatf("Status was %s when writing to register \"%s\" through map \"%s\".",
+                            status.name(), rg.get_full_name(), map.get_full_name()))
+            end
+
+            exp = rg.get() & ~dc_mask
+            yield rg.read(status, val, UVM_FRONTDOOR, map, self)
+            if status != UVM_IS_OK:
+               uvm_error("UVMRegBitBashSeq",
+                       sv.sformatf("Status was %s when reading register \"%s\" through map \"%s\".",
+                           status.name(), rg.get_full_name(), map.get_full_name()))
+
+            val &= ~dc_mask
+            if val != exp:
+                uvm_error("UVMRegBitBashSeq",
+                        sv.sformatf("Writing a %b in bit #%0d of register \"%s\" with initial value 'h%h yielded 'h%h instead of 'h%h",
+                            bit_val, k, rg.get_full_name(), v, val, exp))
+
     #endclass: UVMRegSingleBitBashSeq
 
 uvm_object_utils(UVMRegSingleBitBashSeq)
