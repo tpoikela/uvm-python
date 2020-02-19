d8a8a64664e0af631cd1caeb3c2ff559e44257fb
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-02-19 23:37:45 +0200

    Added test_set_get()

--- unit/test_uvm_reg_field.py
+++ unit/test_uvm_reg_field.py
@@ -2,15 +2,17 @@
 
 import unittest
 
-from uvm.reg.uvm_reg_field import UVMRegField
+from uvm.reg.uvm_reg_field import UVMRegField, NO_RAND_SET
 from uvm.reg.uvm_reg import UVMReg
+from uvm.reg.uvm_reg_model import (UVM_READ, UVM_WRITE, UVM_PREDICT_WRITE,
+    UVM_PREDICT_READ)
 
 
 class TestUVMRegField(unittest.TestCase):
 
-    def create_field(self, name, reg):
+    def create_field(self, name, reg, acc='RW'):
         field = UVMRegField(name)
-        field.configure(parent=reg, size=16, lsb_pos=0, access='RW',
+        field.configure(parent=reg, size=16, lsb_pos=0, access=acc,
             volatile=False, reset=0xABCD, has_reset=True, is_rand=False,
             individually_accessible=False)
         return field
@@ -43,6 +45,27 @@ class TestUVMRegField(unittest.TestCase):
         field.set(0x5555)
         value = field.XupdateX()
         self.assertEqual(value, 0xFFFF)
+
+    def test_set_get(self):
+        for mode in NO_RAND_SET:
+            reg = UVMReg('my_reg', 16, False)
+            field = self.create_field('field_' + mode, reg, acc=mode)
+            field.reset()
+            access = field.get_access()
+            field.set(0x1234)
+            val = field.get()
+            msg = 'Access ' + access + ' OK'
+            predict_msg = 'Predict for ' + access + ' OK'
+            if access in ['RW', 'WRS', 'WRC']:
+                self.assertEqual(val, 0x1234, msg)
+                self.assertTrue(field.predict(0x1234, -1, UVM_PREDICT_WRITE), predict_msg)
+            elif access in ['RO', 'RC', 'RS']:
+                self.assertEqual(val, field.get_reset(), msg)
+                self.assertTrue(field.predict(field.get_reset(), -1, UVM_PREDICT_READ), predict_msg)
+            else:
+                self.assertTrue(field.predict(0x12), predict_msg)
+                val = field.get()
+                self.assertEqual(val, 0x12, 'Val OK after predict with ' + access)
 
 
 if __name__ == '__main__':
