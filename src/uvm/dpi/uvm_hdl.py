e239a5098c86108a5de1eac48262611381154246
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-02-24 23:25:55 +0200

    First version of backdoor access done

--- src/uvm/dpi/uvm_hdl.py
+++ src/uvm/dpi/uvm_hdl.py
@@ -17,8 +17,13 @@ class uvm_hdl():
                 if hasattr(curr_obj, spl):
                     curr_obj = getattr(curr_obj, spl)
                 else:
-                    return 0
+                    print("uvm_hdl_read did not found " + spl)
+                    continue
+            if curr_obj is not None:
+                print("uvm_hdl_read append result " + hex(int(curr_obj)))
                 value.append(int(curr_obj))
+                return 1
+            return 0
         else:
             raise Exception("dut is None. Use uvm_hdl.set_dut(dut) in your @cocotb.test()")
 
@@ -28,12 +33,16 @@ class uvm_hdl():
         if cls.dut is not None:
             curr_obj = cls.dut
             split = path.split('.')
+            print("uvm_hdl_deposit path is " + path)
             for spl in split:
                 if hasattr(curr_obj, spl):
                     curr_obj = getattr(curr_obj, spl)
                 else:
-                    return 0
+                    print("uvm_hdl_deposit did not found " + spl)
+                    continue
+            if curr_obj is not None:
                 curr_obj <= value
                 return 1
+            return 0
         else:
             raise Exception("dut is None. Use uvm_hdl.set_dut(dut) in your @cocotb.test()")
