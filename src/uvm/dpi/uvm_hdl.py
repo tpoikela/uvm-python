587654fc9f3fed473ee8c0d3621d9000a7d00c22
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-02-27 09:34:47 +0200

    Cleaned up prints

--- src/uvm/dpi/uvm_hdl.py
+++ src/uvm/dpi/uvm_hdl.py
@@ -17,10 +17,8 @@ class uvm_hdl():
                 if hasattr(curr_obj, spl):
                     curr_obj = getattr(curr_obj, spl)
                 else:
-                    print("uvm_hdl_read did not found " + spl)
                     continue
             if curr_obj is not None:
-                print("uvm_hdl_read append result " + hex(int(curr_obj)))
                 value.append(int(curr_obj))
                 return 1
             return 0
@@ -33,15 +31,13 @@ class uvm_hdl():
         if cls.dut is not None:
             curr_obj = cls.dut
             split = path.split('.')
-            print("uvm_hdl_deposit path is " + path)
             for spl in split:
                 if hasattr(curr_obj, spl):
                     curr_obj = getattr(curr_obj, spl)
                 else:
-                    print("uvm_hdl_deposit did not found " + spl)
                     continue
             if curr_obj is not None:
-                curr_obj <= value
+                curr_obj.value = value
                 return 1
             return 0
         else:
