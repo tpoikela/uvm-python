6b1b2b66e8223dcaff85d975583fe623404443d7
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-07 22:47:28 +0200

    Added class-level comments and ivar comments

--- src/uvm/tlm1/uvm_tlm_fifo_base.py
+++ src/uvm/tlm1/uvm_tlm_fifo_base.py
@@ -39,110 +39,101 @@ class UVMTLMEvent:
 
 class UVMTLMFIFOBase(UVMComponent):
     """
-
     This class is the base for `UVMTLMFIFO`. It defines the TLM exports
     through which all transaction-based FIFO operations occur. It also defines
     default implementations for each interface method provided by these exports.
 
     The interface methods provided by the `put_export` and the `get_peek_export`
-    are defined and described by <uvm_tlm_if_base #(T1,T2)>.  See the TLM Overview
+    are defined and described by `UVMTLMIfBase`.  See the TLM Overview
     section for a general discussion of TLM interface definition and usage.
 
+    :ivar UVMPutImp put_export:
+
+     The `put_export` provides both the blocking and non-blocking put interface
+     methods to any attached port:
+
+     Example:
+
+     .. code-block:: python
+
+        task put (input T t)
+        function bit can_put ()
+        function bit try_put (input T t)
+
+    Any `put` port variant can connect and send transactions to the FIFO via this
+    export, provided the transaction types match. See `UVMTLMIfBase`
+    for more information on each of the above interface methods.
+
+    :ivar UVMGetPeekImp get_peek_export:
+
+    The `get_peek_export` provides all the blocking and non-blocking get and peek
+    interface methods:
+
+    .. code-block:: python
+        task get (output T t)
+        function bit can_get ()
+        function bit try_get (output T t)
+        task peek (output T t)
+        function bit can_peek ()
+        function bit try_peek (output T t)
+
+    Any `get` or `peek` port variant can connect to and retrieve transactions from
+    the FIFO via this export, provided the transaction types match. See
+    <uvm_tlm_if_base #(T1,T2)> for more information on each of the above interface
+    methods.
+
+    :ivar UVMAnalysisPort put_ap:
+
+    Transactions passed via `put` or `try_put` (via any port connected to the
+    <put_export>) are sent out this port via its `write` method.
+
+    .. code-block:: python
+        def write (T t)
+
+    All connected analysis exports and imps will receive put transactions.
+    See <uvm_tlm_if_base #(T1,T2)> for more information on the `write` interface
+    method.
+
+    :ivar UVMAnalysisPort get_ap:
+
+    Transactions passed via `get`, `try_get`, `peek`, or `try_peek` (via any
+    port connected to the <get_peek_export>) are sent out this port via its
+    `write` method.
+
+    .. code-block:: python
+        def write (T t)
+
+    All connected analysis exports and imps will receive get transactions.
+    See <uvm_tlm_if_base #(T1,T2)> for more information on the `write` method.
+
+    The following are aliases to the above put_export.
+
+    :ivar UVMPutImp blocking_put_export:
+    :ivar UVMPutImp nonblocking_put_export:
+
+    The following are all aliased to the above get_peek_export, which provides
+    the superset of these interfaces.
+
+    :ivar `UVMGetPeekImp` blocking_get_export:
+    :ivar `UVMGetPeekImp` nonblocking_get_export:
+    :ivar `UVMGetPeekImp` get_export:
+    :ivar `UVMGetPeekImp` blocking_peek_export:
+    :ivar `UVMGetPeekImp` nonblocking_peek_export:
+    :ivar `UVMGetPeekImp` peek_export:
+    :ivar `UVMGetPeekImp` blocking_get_peek_export:
+    :ivar `UVMGetPeekImp` nonblocking_get_peek_export:
     """
-
-
-    #  // Port: put_export
-    #  //
-    #  // The ~put_export~ provides both the blocking and non-blocking put interface
-    #  // methods to any attached port:
-    #  //
-    #  //|  task put (input T t)
-    #  //|  function bit can_put ()
-    #  //|  function bit try_put (input T t)
-    #  //
-    #  // Any ~put~ port variant can connect and send transactions to the FIFO via this
-    #  // export, provided the transaction types match. See <uvm_tlm_if_base #(T1,T2)>
-    #  // for more information on each of the above interface methods.
-    #
-    #  uvm_put_imp #(T, this_type) put_export;
-    #
-    #  // Port: get_peek_export
-    #  //
-    #  // The ~get_peek_export~ provides all the blocking and non-blocking get and peek
-    #  // interface methods:
-    #  //
-    #  //|  task get (output T t)
-    #  //|  function bit can_get ()
-    #  //|  function bit try_get (output T t)
-    #  //|  task peek (output T t)
-    #  //|  function bit can_peek ()
-    #  //|  function bit try_peek (output T t)
-    #  //
-    #  // Any ~get~ or ~peek~ port variant can connect to and retrieve transactions from
-    #  // the FIFO via this export, provided the transaction types match. See
-    #  // <uvm_tlm_if_base #(T1,T2)> for more information on each of the above interface
-    #  // methods.
-    #
-    #  uvm_get_peek_imp #(T, this_type) get_peek_export;
-    #
-    #
-    #  // Port: put_ap
-    #  //
-    #  // Transactions passed via ~put~ or ~try_put~ (via any port connected to the
-    #  // <put_export>) are sent out this port via its ~write~ method.
-    #  //
-    #  //|  function void write (T t)
-    #  //
-    #  // All connected analysis exports and imps will receive put transactions.
-    #  // See <uvm_tlm_if_base #(T1,T2)> for more information on the ~write~ interface
-    #  // method.
-    #
-    #  uvm_analysis_port #(T) put_ap;
-    #
-    #
-    #  // Port: get_ap
-    #  //
-    #  // Transactions passed via ~get~, ~try_get~, ~peek~, or ~try_peek~ (via any
-    #  // port connected to the <get_peek_export>) are sent out this port via its
-    #  // ~write~ method.
-    #  //
-    #  //|  function void write (T t)
-    #  //
-    #  // All connected analysis exports and imps will receive get transactions.
-    #  // See <uvm_tlm_if_base #(T1,T2)> for more information on the ~write~ method.
-    #
-    #  uvm_analysis_port #(T) get_ap;
-    #
-    #
-    #  // The following are aliases to the above put_export.
-    #
-    #  uvm_put_imp      #(T, this_type) blocking_put_export;
-    #  uvm_put_imp      #(T, this_type) nonblocking_put_export;
-    #
-    #  // The following are all aliased to the above get_peek_export, which provides
-    #  // the superset of these interfaces.
-    #
-    #  uvm_get_peek_imp #(T, this_type) blocking_get_export;
-    #  uvm_get_peek_imp #(T, this_type) nonblocking_get_export;
-    #  uvm_get_peek_imp #(T, this_type) get_export;
-    #
-    #  uvm_get_peek_imp #(T, this_type) blocking_peek_export;
-    #  uvm_get_peek_imp #(T, this_type) nonblocking_peek_export;
-    #  uvm_get_peek_imp #(T, this_type) peek_export;
-    #
-    #  uvm_get_peek_imp #(T, this_type) blocking_get_peek_export;
-    #  uvm_get_peek_imp #(T, this_type) nonblocking_get_peek_export;
 
     def __init__(self, name, parent=None):
         """
-          The ~name~ and ~parent~ are the normal uvm_component constructor arguments.
-          The ~parent~ should be ~null~ if the uvm_tlm_fifo is going to be used in a
-          statically elaborated construct (e.g., a module). The ~size~ indicates the
+          The `name` and `parent` are the normal `UVMComponent` constructor arguments.
+          The `parent` should be `None` if the `UVMTLMFIFO` is going to be used in a
+          statically elaborated construct (e.g., a module). The `size` indicates the
           maximum size of the FIFO. A value of zero indicates no upper bound.
 
         Args:
-            name:
-            parent:
+            name (str): Name of the component
+            parent (UVMComponent): Parent component.
         """
         UVMComponent.__init__(self, name, parent)
         self.put_export = UVMPutImp("put_export", self)
@@ -165,7 +156,7 @@ class UVMTLMFIFOBase(UVMComponent):
     def build_phase(self, phase):
         """
         Args:
-            phase:
+            phase (UVMPhase):
         """
         self.build()  # for backward compat, won't cause auto-config
         return
