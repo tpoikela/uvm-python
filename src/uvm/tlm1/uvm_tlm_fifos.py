50be7bc4323bb51ce081d7a7c23abf4b3e2aa707
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-07 12:21:00 +0200

    Reformatted comments

--- src/uvm/tlm1/uvm_tlm_fifos.py
+++ src/uvm/tlm1/uvm_tlm_fifos.py
@@ -21,34 +21,32 @@
 #   permissions and limitations under the License.
 #------------------------------------------------------------------------------
 
-import cocotb
 
 from .uvm_tlm_fifo_base import *
 from ..base.uvm_mailbox import UVMMailbox
 
-#------------------------------------------------------------------------------
-#
-# Title: TLM FIFO Classes
-#
-# This section defines TLM-based FIFO classes.
-#
-#------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-#
-# Class: uvm_tlm_fifo#(T)
-#
-# This class provides storage of transactions between two independently running
-# processes. Transactions are put into the FIFO via the ~put_export~.
-# transactions are fetched from the FIFO in the order they arrived via the
-# ~get_peek_export~. The ~put_export~ and ~get_peek_export~ are inherited from
-# the <uvm_tlm_fifo_base #(T)> super class, and the interface methods provided by
-# these exports are defined by the <uvm_tlm_if_base #(T1,T2)> class.
-#
-#------------------------------------------------------------------------------
+"""
+Title: TLM FIFO Classes
+
+This section defines TLM-based FIFO classes.
+
+"""
 
 
 class UVMTLMFIFO(UVMTLMFIFOBase):
+    """
+    Class: UVMTLMFIFO
+
+    This class provides storage of transactions between two independently running
+    processes. Transactions are put into the FIFO via the ~put_export~.
+    transactions are fetched from the FIFO in the order they arrived via the
+    ~get_peek_export~. The ~put_export~ and ~get_peek_export~ are inherited from
+    the <uvm_tlm_fifo_base #(T)> super class, and the interface methods provided by
+    these exports are defined by the <uvm_tlm_if_base #(T1,T2)> class.
+    """
+
+
     type_name = "uvm_tlm_fifo #(T)"
 
     #  // Function: new
@@ -57,7 +55,6 @@ class UVMTLMFIFO(UVMTLMFIFOBase):
     #  // The ~parent~ should be ~null~ if the <uvm_tlm_fifo#(T)> is going to be used in a
     #  // statically elaborated construct (e.g., a module). The ~size~ indicates the
     #  // maximum size of the FIFO; a value of zero indicates no upper bound.
-    #
     def __init__(self, name, parent=None, size=1):
         UVMTLMFIFOBase.__init__(self, name, parent)
         self.m = UVMMailbox(size)
@@ -91,23 +88,22 @@ class UVMTLMFIFO(UVMTLMFIFOBase):
     #  //
     #  // Returns 1 when the number of entries in the FIFO is equal to its <size>,
     #  // 0 otherwise.
-    #
     def is_full(self):
         return (self.m_size != 0) and (self.m.num() == self.m_size)
 
-    
+
     async def put(self, t):
         await self.m.put(t)
         self.put_ap.write(t)
 
-    
+
     async def get(self, t):
         self.m_pending_blocked_gets += 1
         await self.m.get(t)
         self.m_pending_blocked_gets -= 1
         self.get_ap.write(t)
 
-    
+
     async def peek(self, t):
         await self.m.peek(t)
 
@@ -149,46 +145,41 @@ class UVMTLMFIFO(UVMTLMFIFOBase):
         if self.m.num() > 0 and self.m_pending_blocked_gets != 0:
             uvm_report_error("flush failed" ,
                 "there are blocked gets preventing the flush", UVM_NONE)
-    #endclass
 
 
-#------------------------------------------------------------------------------
-#
-# Class: uvm_tlm_analysis_fifo#(T)
-#
-# An analysis_fifo is a <uvm_tlm_fifo#(T)> with an unbounded size and a write interface.
-# It can be used any place a <uvm_analysis_imp> is used. Typical usage is
-# as a buffer between a <uvm_analysis_port> in an initiator component
-# and TLM1 target component.
-#
-#------------------------------------------------------------------------------
 
 class UVMTLMAnalysisFIFO(UVMTLMFIFO):
+    """
+    Class: UVMTLMAnalysisFIFO
+
+    An analysis_fifo is a <uvm_tlm_fifo#(T)> with an unbounded size and a write interface.
+    It can be used any place a <uvm_analysis_imp> is used. Typical usage is
+    as a buffer between a <uvm_analysis_port> in an initiator component
+    and TLM1 target component.
+    """
+
     type_name = "uvm_tlm_analysis_fifo #(T)"
-    #
-    #  // Port: analysis_export #(T)
-    #  //
-    #  // The analysis_export provides the write method to all connected analysis
-    #  // ports and parent exports:
-    #  //
-    #  //|  function void write (T t)
-    #  //
-    #  // Access via ports bound to this export is the normal mechanism for writing
-    #  // to an analysis FIFO.
-    #  // See write method of <uvm_tlm_if_base #(T1,T2)> for more information.
-    #
-    #  uvm_analysis_imp #(T, uvm_tlm_analysis_fifo #(T)) analysis_export
-    #
-    #
+
+
+
     #  // Function: new
     #  //
     #  // This is the standard uvm_component constructor. ~name~ is the local name
     #  // of this component. The ~parent~ should be left unspecified when this
     #  // component is instantiated in statically elaborated constructs and must be
     #  // specified when this component is a child of another UVM component.
-    #
     def __init__(self, name, parent=None):
-        UVMTLMFIFO.__init__(self, name, parent, 0)  # // analysis fifo must be unbounded
+        UVMTLMFIFO.__init__(self, name, parent, 0)  # analysis fifo must be unbounded
+        #  // Port: analysis_export #(T)
+        #  //
+        #  // The analysis_export provides the write method to all connected analysis
+        #  // ports and parent exports:
+        #  //
+        #  //|  def write(self, t)
+        #  //
+        #  // Access via ports bound to this export is the normal mechanism for writing
+        #  // to an analysis FIFO.
+        #  // See write method of <uvm_tlm_if_base #(T1,T2)> for more information.
         self.analysis_export = UVMAnalysisImp("analysis_export", self)
 
     def get_type_name(self):
@@ -196,4 +187,3 @@ class UVMTLMAnalysisFIFO(UVMTLMFIFO):
 
     def write(self, t):
         self.try_put(t)  # unbounded => must succeed
-    #endclass
