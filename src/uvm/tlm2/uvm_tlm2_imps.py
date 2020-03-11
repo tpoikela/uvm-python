d3f1a6de9c661659bbd1a26536eff3ac7c7f0ebe
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-11 09:54:49 +0200

    Corrected UVM_IMP_COMMON macro usage

--- src/uvm/tlm2/uvm_tlm2_imps.py
+++ src/uvm/tlm2/uvm_tlm2_imps.py
@@ -46,8 +46,15 @@ from ..macros.uvm_message_defines import (uvm_error)
 #// Group -- NODOCS -- IMP binding macros
 #//--------------------------
 
-#// Macro -- NODOCS -- `UVM_TLM_NB_TRANSPORT_FW_IMP
-#//
+from uvm.tlm2.uvm_tlm2_ifs import uvm_tlm_sync_e
+from uvm.base.uvm_port_base import UVMPortBase
+from uvm.tlm1.uvm_tlm_imps import UVM_IMP_COMMON
+from uvm.tlm2.uvm_tlm2_defines import UVM_TLM_NB_FW_MASK, UVM_TLM_B_MASK,\
+    UVM_TLM_NB_BW_MASK
+from uvm.macros.uvm_message_defines import uvm_error
+from uvm.base.sv import cat
+import cocotb
+
 #// The macro wraps the forward path call function nb_transport_fw()
 #//
 #// The first call to this method for a transaction marks the initial timing point.
@@ -61,31 +68,18 @@ from ..macros.uvm_message_defines import (uvm_error)
 #// See <TLM2 Interfaces, Ports, Exports and Transport Interfaces Subset>
 #// for more details on the semantics and rules of the nonblocking
 #// transport interface.
-#   
-from uvm.tlm2.uvm_tlm2_ifs import uvm_tlm_sync_e
-from uvm.base.uvm_port_base import UVMPortBase
-from uvm.tlm1.uvm_tlm_imps import UVM_IMP_COMMON
-from uvm.tlm2.uvm_tlm2_defines import UVM_TLM_NB_FW_MASK, UVM_TLM_B_MASK,\
-    UVM_TLM_NB_BW_MASK
-from uvm.macros.uvm_message_defines import uvm_error
-from uvm.base.sv import cat
-import cocotb
-
 def UVM_TLM_NB_TRANSPORT_FW_IMP(imp, T):
     def nb_transport_fw(self, t, p, delay):
-        if (delay == None):
-            uvm_error("UVM/TLM/NULLDELAY", 
-                cat(self.get_full_name(), 
+        if delay is None:
+            uvm_error("UVM/TLM/NULLDELAY",
+                cat(self.get_full_name(),
                    ".nb_transport_fw() called with 'null' delay"))
             return uvm_tlm_sync_e.UVM_TLM_COMPLETED
 
         return getattr(self, imp).nb_transport_fw(t, p, delay)
     setattr(T, "nb_transport_fw", nb_transport_fw)
-#
-#
-#// Macro -- NODOCS -- `UVM_TLM_NB_TRANSPORT_BW_IMP
-#//
-#//
+
+
 #// Implementation of the backward path.
 #// The macro wraps the function called nb_transport_bw().
 #// This function MUST be implemented in the INITIATOR component class.
@@ -122,21 +116,18 @@ def UVM_TLM_NB_TRANSPORT_FW_IMP(imp, T):
 #//|
 #//|    ...
 #//| endclass
-#
 def UVM_TLM_NB_TRANSPORT_BW_IMP(imp, T):
     def nb_transport_bw(self, t, p, delay):
-        if (delay == None):
-            uvm_error("UVM/TLM/NULLDELAY", 
-                cat(self.get_full_name(), 
+        if delay is None:
+            uvm_error("UVM/TLM/NULLDELAY",
+                cat(self.get_full_name(),
                    ".nb_transport_bw() called with 'null' delay"))
             return uvm_tlm_sync_e.UVM_TLM_COMPLETED
         return getattr(self, imp).nb_transport_bw(t, p, delay)
-    
+
     setattr(T, "nb_transport_bw", nb_transport_bw)
-#
-#
-#// Macro -- NODOCS -- `UVM_TLM_B_TRANSPORT_IMP
-#//
+
+
 #// The macro wraps the function b_transport()
 #// Execute a blocking transaction. Once this method returns,
 #// the transaction is assumed to have been executed. Whether
@@ -154,60 +145,48 @@ def UVM_TLM_NB_TRANSPORT_BW_IMP(imp, T):
 #// allows the timing points to be offset from the simulation times
 #// at which the task call and return are executed.
 def UVM_TLM_B_TRANSPORT_IMP(imp, T):
-    
+
     async def b_transport(self, t, delay):
         if delay == None:
-            uvm_error("UVM/TLM/NULLDELAY", 
-                  cat(self.get_full_name(), 
+            uvm_error("UVM/TLM/NULLDELAY",
+                  cat(self.get_full_name(),
                    ".b_transport() called with 'null' delay"))
             return
         await getattr(self, imp).b_transport(t, delay)
     setattr(T, "b_transport", b_transport)
 
-#
-#
+
 #//---------------------------
 #// Group -- NODOCS -- IMP binding classes
 #//---------------------------
-#
-#//----------------------------------------------------------------------
-#// Class -- NODOCS -- uvm_tlm_b_transport_imp
+
 #//
 #// Used like exports, except an additional class parameter specifies
 #// the type of the implementation object.  When the
 #// imp is instantiated the implementation object is bound.
-#//----------------------------------------------------------------------
-
 class UVMTLMBTransportImp(UVMPortBase):
     pass
 
-UVM_IMP_COMMON(UVMTLMBTransportImp, UVM_TLM_B_MASK, "uvm_tlm_b_transport_imp")
+UVMTLMBTransportImp = UVM_IMP_COMMON(UVMTLMBTransportImp, UVM_TLM_B_MASK, "uvm_tlm_b_transport_imp")
 UVM_TLM_B_TRANSPORT_IMP('m_imp', UVMTLMBTransportImp)
 
-#//----------------------------------------------------------------------
-#// Class -- NODOCS -- uvm_tlm_nb_transport_fw_imp
-#//
+
 #// Used like exports, except an additional class parameter specifies
 #// the type of the implementation object.  When the
 #// imp is instantiated the implementation object is bound.
-#//----------------------------------------------------------------------
 class UVMTLMNbTransportFwImp(UVMPortBase):
     pass
 
-UVM_IMP_COMMON(UVMTLMNbTransportFwImp, UVM_TLM_NB_FW_MASK, "uvm_tlm_nb_transport_fw_imp")
+UVMTLMNbTransportFwImp = UVM_IMP_COMMON(UVMTLMNbTransportFwImp, UVM_TLM_NB_FW_MASK, "uvm_tlm_nb_transport_fw_imp")
 UVM_TLM_NB_TRANSPORT_FW_IMP('m_imp', UVMTLMNbTransportFwImp)
 
-#//----------------------------------------------------------------------
-#// Class -- NODOCS -- uvm_tlm_nb_transport_bw_imp
-#//
+
 #// Used like exports, except an additional class parameter specifies
 #// the type of the implementation object.  When the
 #// imp is instantiated the implementation object is bound.
-#//----------------------------------------------------------------------
-
 class UVMTLMNbTransportBwImp(UVMPortBase):
     pass
 
-UVM_IMP_COMMON(UVMTLMNbTransportBwImp, UVM_TLM_NB_BW_MASK, "uvm_tlm_nb_transport_bw_imp")
+UVMTLMNbTransportBwImp = UVM_IMP_COMMON(UVMTLMNbTransportBwImp, UVM_TLM_NB_BW_MASK, "uvm_tlm_nb_transport_bw_imp")
 UVM_TLM_NB_TRANSPORT_BW_IMP('m_imp', UVMTLMNbTransportBwImp)
 
