281021a9718d56c0a0408e9848a697f8bf29829c
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-14 09:10:03 +0200

    Added tests for unpack_field_int

--- unit/test_uvm_packer.py
+++ unit/test_uvm_packer.py
@@ -36,6 +36,8 @@ class TestUVMPacker(unittest.TestCase):
         arr = [0] * 4
         new_bytes = packer.unpack_bytes(arr)
         self.assertEqual(new_bytes, arr_bytes)
+        my_bytes = packer.get_bytes()
+        self.assertEqual(my_bytes, arr_bytes)
 
 
     def test_pack_unpack_ints(self):
@@ -49,6 +51,24 @@ class TestUVMPacker(unittest.TestCase):
         arr = [0] * 4
         new_ints = packer.unpack_ints(arr)
         self.assertEqual(new_ints, arr_int)
+
+        my_ints = packer.get_ints()
+        self.assertEqual(my_ints, arr_int)
+
+        packer.reset()
+        packer.pack_ints(arr_int, 4 * 32)
+        packer.set_packed_size()
+        self.assertEqual(packer.unpack_field_int(128), 0x000000AB000000890000456700000123)
+
+        packer.reset()
+        packer.pack_ints(arr_int, 4 * 32)
+        packer.set_packed_size()
+        self.assertEqual(packer.unpack_field_int(64), 0x0000456700000123)
+
+        packer.reset()
+        packer.pack_ints(arr_int, 4 * 32)
+        packer.set_packed_size()
+        self.assertEqual(packer.unpack_field_int(32), 0x00000123)
 
 
 if __name__ == '__main__':
