7f7df9a79d0af59858944820d67289a12afd1053
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-22 15:58:16 +0200

    Implemented do_predict and pre-functions

--- src/uvm/reg/uvm_reg_fifo.py
+++ src/uvm/reg/uvm_reg_fifo.py
@@ -21,25 +21,27 @@
 #//
 
 import cocotb
-from uvm.reg.uvm_reg import UVMReg
-from uvm.reg.uvm_reg_field import UVMRegField
-from uvm.reg.uvm_reg_model import (UVM_CHECK, UVM_DEFAULT_PATH)
+from .uvm_reg import UVMReg
+from .uvm_reg_field import UVMRegField
+from .uvm_reg_model import (UVM_CHECK, UVM_DEFAULT_PATH, UVM_NOT_OK,
+    UVM_PREDICT_WRITE, UVM_PREDICT_DIRECT, UVM_PREDICT_READ)
+from ..macros import uvm_warning, uvm_error
+from ..base.sv import sv
 
-
-#//------------------------------------------------------------------------------
-#// Class: UVMRegFIFO
-#//
-#// This special register models a DUT FIFO accessed via write/read,
-#// where writes push to the FIFO and reads pop from it.
-#//
-#// Backdoor access is not enabled, as it is not yet possible to force
-#// complete FIFO state, i.e. the write and read indexes used to access
-#// the FIFO data.
-#//
-#//------------------------------------------------------------------------------
 
 
 class UVMRegFIFO(UVMReg):
+    """
+    Class: UVMRegFIFO
+
+    This special register models a DUT FIFO accessed via write/read,
+    where writes push to the FIFO and reads pop from it.
+
+    Backdoor access is not enabled, as it is not yet possible to force
+    complete FIFO state, i.e. the write and read indexes used to access
+    the FIFO data.
+    """
+
 
 
     #    // Variable: fifo
@@ -52,7 +54,7 @@ class UVMRegFIFO(UVMReg):
 
 
     #    constraint valid_fifo_size {
-    #      fifo.size() <= m_size
+    #      len(self.fifo) <= m_size
     #    }
 
 
@@ -75,7 +77,7 @@ class UVMRegFIFO(UVMReg):
     #    // Funtion: build
     #    //
     #    // Builds the abstract FIFO register object. Called by
-    #    // the instantiating block, a <uvm_reg_block> subtype.
+    #    // the instantiating block, a `UVMRegBlock` subtype.
     #    //
     def build(self):
         self.value = UVMRegField.type_id.create("value")
@@ -84,7 +86,7 @@ class UVMRegFIFO(UVMReg):
 
     #    // Function: set_compare
     #    //
-    #    // Sets the compare policy during a mirror (read) of the DUT FIFO. 
+    #    // Sets the compare policy during a mirror (read) of the DUT FIFO.
     #    // The DUT read value is checked against its mirror only when both the
     #    // ~check~ argument in the <mirror()> call and the compare policy
     #    // for the field is <UVM_CHECK>.
@@ -118,10 +120,10 @@ class UVMRegFIFO(UVMReg):
     #    //--------------
 
     #    //  Function: write
-    #    // 
+    #    //
     #    //  Pushes the given value to the DUT FIFO. If auto-prediction is enabled,
     #    //  the written value is also pushed to the abstract FIFO before the
-    #    //  call returns. If auto-prediction is not enabled (via 
+    #    //  call returns. If auto-prediction is not enabled (via
     #    //  `UVMRegMap.set_auto_predict`), the value is pushed to abstract
     #    //  FIFO only when the write operation is observed on the target bus.
     #    //  This mode requires using the <uvm_reg_predictor> class.
@@ -163,7 +165,7 @@ class UVMRegFIFO(UVMReg):
     async def update(self, status, path=UVM_DEFAULT_PATH, map=None, parent=None,
             prior=-1, extension=None, fname="", lineno=0):
         upd = 0x0
-        if (self.m_set_cnt == 0 or len(self.fifo) == 0):
+        if self.m_set_cnt == 0 or len(self.fifo) == 0:
             return
         self.m_update_in_progress = 1
         i = len(self.fifo)-self.m_set_cnt
@@ -176,15 +178,13 @@ class UVMRegFIFO(UVMReg):
                 await self.write(status, self.fifo[i], path, map, parent,prior,extension,fname,lineno)
             i += 1
             self.m_set_cnt -= 1
-          
-
         self.m_update_in_progress = 0
 
 
     #    // Function: mirror
     #    //
     #    // Reads the next value out of the DUT FIFO. If auto-prediction is
-    #    // enabled, the frontmost value in abstract FIFO is popped. If 
+    #    // enabled, the frontmost value in abstract FIFO is popped. If
     #    // the ~check~ argument is set and comparison is enabled with
     #    // <set_compare()>.
 
@@ -196,7 +196,8 @@ class UVMRegFIFO(UVMReg):
     #    //
     def get(self, fname="", lineno=0):
         #return fifo.pop(0)();
-        return self.fifo[0]
+        val = self.fifo[0]
+        return val
 
 
     #    // Function: do_predict
@@ -204,12 +205,12 @@ class UVMRegFIFO(UVMReg):
     #    // Updates the abstract (mirror) FIFO based on <write()> and
     #    // <read()> operations.  When auto-prediction is on, this method
     #    // is called before each read, write, peek, or poke operation returns.
-    #    // When auto-prediction is off, this method is called by a 
-    #    // <uvm_reg_predictor> upon receipt and conversion of an observed bus
+    #    // When auto-prediction is off, this method is called by a
+    #    // `UVMRegPredictor` upon receipt and conversion of an observed bus
     #    // operation to this register.
     #    //
     #    // If a write prediction, the observed
-    #    // write value is pushed to the abstract FIFO as long as it is 
+    #    // write value is pushed to the abstract FIFO as long as it is
     #    // not full and the operation did not originate from an <update()>.
     #    // If a read prediction, the observed read value is compared
     #    // with the frontmost value in the abstract FIFO if <set_compare()>
@@ -218,38 +219,25 @@ class UVMRegFIFO(UVMReg):
     #    virtual function void do_predict(uvm_reg_item      rw,
     #                                     uvm_predict_e     kind = UVM_PREDICT_DIRECT,
     #                                     uvm_reg_byte_en_t be = -1)
-    #
-    #      super().do_predict(rw,kind,be)
-    #
-    #      if (rw.status == UVM_NOT_OK)
-    #        return
-    #
-    #      case (kind)
-    #
-    #        UVM_PREDICT_WRITE,
-    #        UVM_PREDICT_DIRECT:
-    #        begin
-    #           if (fifo.size() != m_size  and  !m_update_in_progress)
-    #             fifo.append(self.value.value)
-    #        end
-    #
-    #        UVM_PREDICT_READ:
-    #        begin
-    #           uvm_reg_data_t value = rw.value[0] & ((1 << get_n_bits())-1)
-    #           uvm_reg_data_t mirror_val
-    #           if (fifo.size() == 0):
-    #             return
-    #           end
-    #           mirror_val = fifo.pop(0)()
-    #           if (self.value.get_compare() == UVM_CHECK  and  mirror_val != value):
-    #              uvm_warning("MIRROR_MISMATCH",
-    #               sv.sformatf("Observed DUT read value 'h%0h != mirror value 'h%0h",value,mirror_val))
-    #           end
-    #        end
-    #
-    #      endcase
-    #
-    #    endfunction
+    def do_predict(self, rw, kind=UVM_PREDICT_DIRECT, be=-1):
+        super().do_predict(rw,kind,be)
+
+        if rw.status == UVM_NOT_OK:
+            return
+
+        if kind in [UVM_PREDICT_WRITE, UVM_PREDICT_DIRECT]:
+            if (len(self.fifo) != self.m_size) and (not self.m_update_in_progress):
+                self.fifo.append(self.value.value)
+        elif kind == UVM_PREDICT_READ:
+            value = rw.value[0] & ((1 << self.get_n_bits())-1)
+            mirror_val = 0x0
+            if (len(self.fifo) == 0):
+                return
+            mirror_val = self.fifo.pop(0)
+            if (self.value.get_compare() == UVM_CHECK and mirror_val != value):
+                uvm_warning("MIRROR_MISMATCH", sv.sformatf(
+                    "Observed DUT read value 'h%0h != mirror value 'h%0h, FIFO: %s",
+                    value, mirror_val, str(self.fifo)))
 
 
     #    // Group: Special Overrides
@@ -261,39 +249,30 @@ class UVMRegFIFO(UVMReg):
     #    // attempt a write to a full FIFO or a write while an update is still
     #    // pending. An update is pending after one or more calls to <set()>.
     #    // If in your application the DUT allows writes to a full FIFO, you
-    #    // must override ~pre_write~ as appropriate.
-    #    //
-    #@cocotb.coroutine
-    #    def pre_write(self,uvm_reg_item rw)
-    #      if (m_set_cnt  and  !m_update_in_progress):
-    #        uvm_error("Needs Update","Must call update() after set() and before write()")
-    #        rw.status = UVM_NOT_OK
-    #        return
-    #      end
-    #      if (fifo.size() >= m_size  and  !m_update_in_progress):
-    #        uvm_error("FIFO Full","Write to full FIFO ignored")
-    #        rw.status = UVM_NOT_OK
-    #        return
-    #      end
-    #    endtask
+    #    // must override `UVMRegFIFO.pre_write` as appropriate.
+    async def pre_write(self, rw):
+        if self.m_set_cnt > 0 and not self.m_update_in_progress:
+            uvm_error("Needs Update","Must call update() after set() and before write()")
+            rw.status = UVM_NOT_OK
+            return
+
+        if (len(self.fifo) >= self.m_size and not self.m_update_in_progress):
+            uvm_error("FIFO Full","Write to full FIFO ignored")
+            rw.status = UVM_NOT_OK
+            return
 
 
     #    // Task: pre_read
     #    //
     #    // Special post-processing for a <write()> or <update()>.
     #    // Aborts the operation if the internal FIFO is empty. If in your application
-    #    // the DUT does not behave this way, you must override ~pre_write~ as
+    #    // the DUT does not behave this way, you must override `UVMRegFIFO.pre_read` as
     #    // appropriate.
-    #    //
-    #    //
-    #@cocotb.coroutine
-    #    def pre_read(self,uvm_reg_item rw)
-    #      // abort if fifo empty
-    #      if (fifo.size() == 0):
-    #        rw.status = UVM_NOT_OK
-    #        return
-    #      end
-    #    endtask
+    async def pre_read(self, rw):
+        # abort if fifo empty
+        if len(self.fifo) == 0:
+            rw.status = UVM_NOT_OK
+            return
 
 
     def post_randomize(self):
