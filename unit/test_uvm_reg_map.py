21f5f7b0b0d76493a5d0199ade9388b9736a102a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-22 08:42:59 +0200

    Added test_error_conditions

--- unit/test_uvm_reg_map.py
+++ unit/test_uvm_reg_map.py
@@ -20,9 +20,12 @@ class TestUVMRegMap(unittest.TestCase):
         self.assertEqual(r1.get_offset(), 4)
         r1.set_offset(reg_map, 16)
         self.assertEqual(r1.get_offset(), 16)
+        reg_map.reset(kind="SOFT")
+
 
     def test_add_parent_map(self):
         pass
+
 
     def test_get_physical_addresses(self):
         # reg_map = UVMRegMap()
@@ -50,3 +53,13 @@ class TestUVMRegMap(unittest.TestCase):
         bus_width = reg_map.get_physical_addresses(mem_offset, 0, nbytes, addrs)
         self.assertEqual(addrs[0], mem_offset)
         self.assertEqual(bus_width, 4)
+
+
+    def test_error_conditions(self):
+        reg_blk = UVMRegBlock('my_blk2')
+        reg_map = reg_blk.create_map("default_map2", 0x0, 4,
+            UVM_LITTLE_ENDIAN)
+        reg_map.set_sequencer(None, None)
+        reg_map.set_submap_offset(None, 0x123)
+        err = reg_map.get_submap_offset(None)
+        self.assertEqual(err, -1)
