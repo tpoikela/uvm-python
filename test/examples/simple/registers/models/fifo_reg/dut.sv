290903de3993937db9e56b1a963d6f4d23205b55
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-22 08:40:40 +0200

    Expanded apb if

--- test/examples/simple/registers/models/fifo_reg/dut.sv
+++ test/examples/simple/registers/models/fifo_reg/dut.sv
@@ -2,6 +2,7 @@
 // -------------------------------------------------------------
 //    Copyright 2004-2011 Synopsys, Inc.
 //    Copyright 2010 Mentor Graphics Corporation
+//    Copyright 2020 Tuomas Poikela (tpoikela)
 //    All Rights Reserved Worldwide
 // 
 //    Licensed under the Apache License, Version 2.0 (the
@@ -20,8 +21,22 @@
 // -------------------------------------------------------------
 // 
 
+/* Test DUT which has one register FIFO. */
 
-module dut #(int BASE_ADDR='h0) (apb_if apb, input bit rst);
+`timescale 1ns/1ns
+
+module dut #(
+   parameter int BASE_ADDR='h0
+) (
+   input apb_pclk,
+   input bit rst,
+   input wire [31:0] apb_paddr,
+   input        apb_psel,
+   input        apb_penable,
+   input        apb_pwrite,
+   output [31:0] apb_prdata,
+   input [31:0] apb_pwdata
+);
 
 reg [31:0] fifo[8];
 reg [2:0]  w_idx;
@@ -33,27 +48,27 @@ wire in_range;
 
 wire [31:0] pr_addr;
 
+assign in_range = (apb_paddr - BASE_ADDR) == 0; // only 1 reg
+assign pr_addr = apb_paddr - BASE_ADDR;
 
-assign in_range = (apb.paddr - BASE_ADDR) == 0; // only 1 reg
-assign pr_addr = apb.paddr - BASE_ADDR;
+assign apb_prdata = (apb_psel && apb_penable && !apb_pwrite && in_range) ? pr_data : 'z;
 
-assign apb.prdata = (apb.psel && apb.penable && !apb.pwrite && in_range) ? pr_data : 'z;
-
-always @ (posedge apb.pclk)
+always @ (posedge apb_pclk)
   begin
    if (rst) begin
-      fifo <= '{default:'h0};
+       foreach (fifo[i])
+           fifo[i] <= 'h0;
       w_idx <= 0;
       r_idx <= 0;
       used <= 0;
    end
    else begin
       // Wait for a SETUP+READ or ENABLE+WRITE cycle
-      if (apb.psel == 1'b1 && apb.penable == apb.pwrite && pr_addr == 'h0) begin
+      if (apb_psel == 1'b1 && apb_penable == apb_pwrite && pr_addr == 'h0) begin
          pr_data <= 32'h0;
-         if (apb.pwrite) begin
+         if (apb_pwrite) begin
            if (used != 8) begin
-             fifo[w_idx] <= apb.pwdata;
+             fifo[w_idx] <= apb_pwdata;
              w_idx       <= w_idx + 1;
              used        <= used + 1;
            end
