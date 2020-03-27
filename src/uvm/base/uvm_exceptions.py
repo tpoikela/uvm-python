077bcf7dc5720c444061933f0fe0524eece927c7
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-28 00:11:00 +0200

    Added uvm_exceptions

---
+++ src/uvm/base/uvm_exceptions.py
@@ -0,0 +1,15 @@
+
+class RandomizeError(Exception):
+    """
+    Exception which is thrown when constrained randomization
+    fails
+    """
+    pass
+
+
+class TLMError(Exception):
+    """
+    Exception which is thrown when nonblocking TLM method fails
+    fails
+    """
+    pass
