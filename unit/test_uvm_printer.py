67774dc328c57c9643c3bec17be4e7b8873eae50
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-28 10:27:20 +0200

    Added test_print_field_int

--- unit/test_uvm_printer.py
+++ unit/test_uvm_printer.py
@@ -5,6 +5,8 @@ from uvm.base.uvm_printer import (UVMPrinter, UVMLinePrinter, UVMTreePrinter,
     UVMTablePrinter, UVMJSONPrinter)
 
 from uvm.base.uvm_object import UVMObject
+from uvm.base.uvm_object_globals import UVM_DEC
+
 
 class TestUVMPrinter(unittest.TestCase):
 
@@ -16,10 +18,18 @@ class TestUVMPrinter(unittest.TestCase):
         printer.print_array_header("xxx", 10)
         row_info = printer.m_rows[0]
 
+
 class TestUVMTablePrinter(unittest.TestCase):
+
+    def test_print_field_int(self):
+        tprinter = UVMTablePrinter()
+        tprinter.print_field_int("my_number", 0x5678, 32)
+        result = tprinter.emit()
+        self.assertRegex(result, 'number.*5678')
 
     def test_table_printer(self):
         tprinter = UVMTablePrinter()
+        tprinter.knobs.default_radix = UVM_DEC
         tprinter.print_field_int("number", 1234, 32)
         tprinter.print_string("my_string", "xxx yyy")
         tprinter.print_real("my_real", 1.2345)
@@ -33,6 +43,7 @@ class TestUVMLinePrinter(unittest.TestCase):
 
     def test_line_printer(self):
         lprinter = UVMLinePrinter()
+        lprinter.knobs.default_radix = UVM_DEC
         lprinter.print_field_int("number", 1234, 32)
         lprinter.print_string("my_string", "xxx yyy")
         lprinter.print_real("my_real", 1.2345)
@@ -41,10 +52,9 @@ class TestUVMLinePrinter(unittest.TestCase):
         result = lprinter.emit()
         self.assertRegex(result, 'number.*1234')
         self.assertRegex(result, 'my_string.*xxx yyy')
-        self.assertRegex(result, 'my_real: 1\.2345')
+        self.assertRegex(result, 'my_real: 1.2345')
         self.assertRegex(result, 'test obj123')
 
 
 if __name__ == '__main__':
     unittest.main()
-
