c2a3f32176f78807972f637e64d5221efc24bfa8
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-28 10:27:05 +0200

    Added test_uvm_bit_stream_to_string

--- unit/test_uvm_misc.py
+++ unit/test_uvm_misc.py
@@ -1,6 +1,10 @@
 
 import unittest
-from uvm.base.uvm_misc import *
+from uvm.base.uvm_object_globals import (
+    UVM_DEC, UVM_BIN, UVM_HEX, UVM_OCT)
+from uvm.base.uvm_misc import (
+    uvm_leaf_scope,
+    uvm_bitstream_to_string)
 
 
 class TestUVMMisc(unittest.TestCase):
@@ -17,6 +21,20 @@ class TestUVMMisc(unittest.TestCase):
         self.assertEqual(scope, "leaf")
         #scope = uvm_leaf_scope("a#b#leaf", "#")
         #self.assertEqual(scope, "leaf")
+
+    def test_uvm_bitstream_to_string(self):
+        radix = [UVM_DEC, UVM_BIN, UVM_HEX, UVM_OCT]
+        for rr in radix:
+            num_str = uvm_bitstream_to_string(255, 8, rr)
+            if rr == UVM_DEC:
+                self.assertEqual(num_str, "{}".format(255))
+            elif rr == UVM_HEX:
+                self.assertEqual(num_str, "{:X}".format(255))
+            elif rr == UVM_BIN:
+                self.assertEqual(num_str, "{:b}".format(255))
+
+        num_str = uvm_bitstream_to_string(-245, 8, UVM_DEC)
+        self.assertEqual(num_str, "-245")
 
 
 if __name__ == '__main__':
