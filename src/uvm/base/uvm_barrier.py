a9d94f84e2aa8bb2dde8bb7ef80f16914c8ba8f6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-28 11:49:47 +0200

    Added uvm_barrier.py

---
+++ src/uvm/base/uvm_barrier.py
@@ -0,0 +1,183 @@
+#//
+#//------------------------------------------------------------------------------
+#//   Copyright 2007-2010 Mentor Graphics Corporation
+#//   Copyright 2007-2010 Cadence Design Systems, Inc.
+#//   Copyright 2010 Synopsys, Inc.
+#//   Copyright 2019-2020 Tuomas Poikela (tpoikela)
+#//   All Rights Reserved Worldwide
+#//
+#//   Licensed under the Apache License, Version 2.0 (the
+#//   "License"); you may not use this file except in
+#//   compliance with the License.  You may obtain a copy of
+#//   the License at
+#//
+#//       http://www.apache.org/licenses/LICENSE-2.0
+#//
+#//   Unless required by applicable law or agreed to in
+#//   writing, software distributed under the License is
+#//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#//   CONDITIONS OF ANY KIND, either express or implied.  See
+#//   the License for the specific language governing
+#//   permissions and limitations under the License.
+#//------------------------------------------------------------------------------
+
+
+from .uvm_object import UVMObject
+from .uvm_event import UVMEvent
+
+
+class UVMBarrier(UVMObject):
+    """
+    The UVMBarrier class provides a multiprocess synchronization mechanism. Note
+    that this is only for cocotb-based simulation, not real multiprocessing.
+    It enables a set of processes to block until the desired number of processes
+    get to the synchronization point, at which time all of the processes are
+    released.
+    """
+
+
+    #  // Function: new
+    #  //
+    #  // Creates a new barrier object.
+    #
+    def __init__(self, name="", threshold=0):
+        super().__init__(name)
+        self.m_event = UVMEvent({"barrier_",name})
+        self.threshold = threshold
+        self.num_waiters = 0
+        self.auto_reset = 1
+        self.at_threshold = 0
+
+
+
+    #  // Task: wait_for
+    #  //
+    #  // Waits for enough processes to reach the barrier before continuing. 
+    #  //
+    #  // The number of processes to wait for is set by the <set_threshold> method.
+    async def wait_for(self):
+        if self.at_threshold:
+            return
+
+        self.num_waiters += 1
+
+        if self.num_waiters >= self.threshold:
+            if not self.auto_reset:
+                self.at_threshold=1
+            self.m_trigger()
+            return
+        
+        await self.m_event.wait_trigger()
+
+
+    #  // Function: reset
+    #  //
+    #  // Resets the barrier. This sets the waiter count back to zero. 
+    #  //
+    #  // The threshold is unchanged. After reset, the barrier will force processes
+    #  // to wait for the threshold again. 
+    #  //
+    #  // If the ~wakeup~ bit is set, any currently waiting processes will
+    #  // be activated.
+    def reset(self, wakeup=1):
+        self.at_threshold = 0
+        if self.num_waiters:
+            if wakeup:
+                self.m_event.trigger()
+            else:
+                self.m_event.reset()
+        self.num_waiters = 0
+
+
+    #  // Function: set_auto_reset
+    #  //
+    #  // Determines if the barrier should reset itself after the threshold is
+    #  // reached. 
+    #  //
+    #  // The default is on, so when a barrier hits its threshold it will reset, and
+    #  // new processes will block until the threshold is reached again. 
+    #  //
+    #  // If auto reset is off, then once the threshold is achieved, new processes
+    #  // pass through without being blocked until the barrier is reset.
+    def set_auto_reset(self, value=1):
+        self.at_threshold = 0
+        self.auto_reset = value
+
+
+    #  // Function: set_threshold
+    #  //
+    #  // Sets the process threshold. 
+    #  //
+    #  // This determines how many processes must be waiting on the barrier before
+    #  // the processes may proceed. 
+    #  //
+    #  // Once the ~threshold~ is reached, all waiting processes are activated. 
+    #  //
+    #  // If ~threshold~ is set to a value less than the number of currently
+    #  // waiting processes, then the barrier is reset and waiting processes are
+    #  // activated.
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+        if threshold <= self.num_waiters:
+            self.reset(1)
+
+
+
+    #  // Function: get_threshold
+    #  //
+    #  // Gets the current threshold setting for the barrier.
+    def get_threshold(self):
+        return self.threshold
+
+
+    #  // Function: get_num_waiters
+    #  //
+    #  // Returns the number of processes currently waiting at the barrier.
+    #
+    def get_num_waiters(self):
+        return self.num_waiters
+
+
+    #  // Function: cancel
+    #  //
+    #  // Decrements the waiter count by one. This is used when a process that is
+    #  // waiting on the barrier is killed or activated by some other means.
+    def cancel(self):
+        self.m_event.cancel()
+        self.num_waiters = self.m_event.get_num_waiters()
+
+
+    type_name = "UVMBarrier"
+    #
+    def create(self, name=""):
+        v = UVMBarrier(name)
+        return v
+
+    def get_type_name(self):
+        return UVMBarrier.type_name
+
+
+    def m_trigger(self):
+        self.m_event.trigger()
+        self.num_waiters=0
+        await Timer(0, "NS")  # self process was last to wait; allow other procs to resume first
+
+
+    def do_print(self, printer):
+        printer.print_field_int("threshold", threshold, sv.bits(threshold), UVM_DEC, ".", "int")
+        printer.print_field_int("num_waiters", num_waiters, sv.bits(num_waiters), UVM_DEC, ".", "int")
+        printer.print_field_int("at_threshold", at_threshold, sv.bits(at_threshold), UVM_BIN, ".", "bit")
+        printer.print_field_int("auto_reset", auto_reset, sv.bits(auto_reset), UVM_BIN, ".", "bit")
+
+
+    #  def do_copy(self,uvm_object rhs):
+    #    UVMBarrier b
+    #    super().do_copy(rhs)
+    #    if(!sv.cast(b, rhs)  or  (b is None)) return
+    #
+    #    threshold = b.threshold
+    #    num_waiters = b.num_waiters
+    #    at_threshold = b.at_threshold
+    #    auto_reset = b.auto_reset
+    #    m_event = b.m_event
+    #  endfunction  
