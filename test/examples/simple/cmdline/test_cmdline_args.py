84cb11ef85bbf5b269d4ea04f1d11240b1d0dc42
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-03-29 16:50:26 +0300

    Added max quit count testing.

--- test/examples/simple/cmdline/test_cmdline_args.py
+++ test/examples/simple/cmdline/test_cmdline_args.py
@@ -21,9 +21,13 @@ import cocotb
 from cocotb.triggers import Timer
 
 from uvm import run_test, UVMTest, UVMComponent
+from uvm.base.uvm_root import UVMRoot
 from uvm.base.uvm_object_globals import *
 from uvm.base.uvm_debug import uvm_debug, UVMDebug
-from uvm.macros import uvm_component_utils, uvm_fatal, uvm_error
+from uvm.macros import (uvm_component_utils, uvm_fatal, uvm_error,
+    uvm_info, uvm_component_utils_begin, uvm_component_utils_end,
+    uvm_field_string, uvm_field_int)
+
 
 
 class DummyTest(UVMTest):
@@ -41,6 +45,7 @@ class CmdLineTest(UVMTest):
         super().__init__(name, parent)
         self.comp = None
         self.comp = MyComponent("my_comp", self)
+        self.check_phase_ran = False
 
     def build_phase(self, phase):
         super().build_phase(phase)
@@ -52,6 +57,7 @@ class CmdLineTest(UVMTest):
 
 
     def check_phase(self, phase):
+        self.check_phase_ran = True
         if self.comp.verb_checked_build_phase is False:
             uvm_fatal("CmdLineTest", "No verb checked in my_comp")
 
@@ -70,24 +76,55 @@ class TestMaxQuitCount(CmdLineTest):
         await Timer(100, 'ns')
         for i in range(10):
             if i < 9:
-                uvm_error("TestMaxQuitCountInfo", "Info " + str(i))
+                uvm_error("TestMaxQuitCountInfo", "Error " + str(i))
+            else:
+                # Need to catch this because max_quit_count reached
+                try:
+                    uvm_error("TestMaxQuitCountInfo", "Last Error " + str(i))
+                except Exception as e:
+                    uvm_info("TestMaxQuitCount_CAUGHT", "Exception: " + str(e),
+                        UVM_NONE)
+                    self.caught = True
         await Timer(100, 'ns')
-        uvm_fatal("TestMaxQuitCount", "This must not be reached")
         phase.drop_objection(self)
 
+
+    def check_phase(self, phase):
+        self.check_phase_ran = True
+        if self.caught is False:
+            uvm_fatal("TestMaxQuitCount TEST FAILED",
+                "Did not catch the exception correctly")
+        if self.comp.tag != 'matched_correctly':
+            uvm_fatal("TestMaxQuitCount TEST FAILED",
+                "+uvm_set_config_string did not match correctly")
 
 uvm_component_utils(TestMaxQuitCount)
 
 
+#------------------------------
+# Sub-components for the tests
+#------------------------------
+
+
 class MyComponent(UVMComponent):
+    """
+    Sub-component created by the tests.
+    """
 
     def __init__(self, name, parent):
         super().__init__(name, parent)
         self.verb_checked_build_phase = False
         self.old_rh = self.m_rh
+        self.tag = "<not set>"
+        self.hex_to_match = 0
+        self.oct_to_match = 0
+        self.dec_to_match = 0
+        self.str_with_ws = "<not set>"
 
 
     def build_phase(self, phase):
+        # Need to be called, in order to trigger auto-config
+        super().build_phase(phase)
         if self.uvm_report_enabled(UVM_DEBUG):
             uvm_fatal("MyComponent", "Verbosity UVM_DEBUG must not be enabled")
 
@@ -124,10 +161,31 @@ class MyComponent(UVMComponent):
 
     def check_phase(self, phase):
         uvm_fatal("FAT2WARN", "Severity should change UVM_FATAL->UVM_WARN")
-# uvm_component_utils(MyComponent)
+        if self.hex_to_match != 0x789:
+            uvm_fatal("HEX_NUM_CONFIG_INT_FATAL",
+                "hex_to_match not auto-configured correctly")
+        if self.oct_to_match != int('456', 8):
+            uvm_fatal("OCT_NUM_CONFIG_INT_FATAL",
+                "oct_to_match not auto-configured correctly")
+        if self.dec_to_match < 1000000000:
+            uvm_fatal("DEC_NUM_CONFIG_INT_FATAL",
+                "dec_to_match not auto-configured correctly")
+
+uvm_component_utils_begin(MyComponent)
+uvm_field_string('tag')
+uvm_field_string('str_with_ws')
+uvm_field_int('hex_to_match')
+uvm_field_int('oct_to_match')
+uvm_field_int('dec_to_match')
+uvm_component_utils_end(MyComponent)
 
 
 @cocotb.test()
 async def test_cmdline_args(dut):
     await run_test()
     await Timer(1001, 'ns')
+
+    uvm_root = UVMRoot.get()
+    test_top = uvm_root.find('uvm_test_top$')
+    if test_top.check_phase_ran is False:
+        raise Exception('*** TEST FAILED ***\nCheck phase did not run in test_top')
