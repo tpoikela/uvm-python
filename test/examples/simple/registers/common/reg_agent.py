2c12a89a5c8cfd3b708ede62b5b249e7c59052f8
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-04-12 00:29:03 +0300

    Added bit_en mask for driver

--- test/examples/simple/registers/common/reg_agent.py
+++ test/examples/simple/registers/common/reg_agent.py
@@ -64,29 +64,24 @@ class reg_rw(UVMSequenceItem):
 uvm_object_utils(reg_rw)
 
 
-class reg_sequencer(UVMSequencer):  #(reg_rw)
+class reg_sequencer(UVMSequencer):
 
     def __init__(self, name, parent=None):
         super().__init__(name, parent)
 
-    #endclass : reg_sequencer
+
 uvm_component_utils(reg_sequencer)
 
 
 class reg_monitor(UVMMonitor):
-    #
-    #   uvm_analysis_port#(reg_rw) ap
 
     def __init__(self, name, parent=None):
         UVMMonitor.__init__(self, name, parent)
         self.ap = UVMAnalysisPort("ap", self)
-    #endclass: reg_monitor
 
 
 class reg_driver(UVMComponent):
 
-    #   uvm_seq_item_pull_port #(reg_rw) seqr_port
-    #   local uvm_component m_parent
 
     def __init__(self, name, parent=None):
         UVMComponent.__init__(self, name, parent)
@@ -94,11 +89,15 @@ class reg_driver(UVMComponent):
         self.seqr_port = UVMSeqItemPullPort("seqr_port",self)
         self.T = reg_rw
         self.dut = None
-        self.n_bits = 32
-        #endfunction
+        self.set_nbits(32)
 
+    def set_nbits(self, nbits):
+        self.n_bits = nbits
+        self.bit_en = 0x0
+        for n in range(self.n_bits):
+            self.bit_en <<= 1
+            self.bit_en |= 0x1
 
-    
     async def run_phase(self, phase):
         mon = self.m_parent.get_child("mon")
         while True:
@@ -109,16 +108,17 @@ class reg_driver(UVMComponent):
             mon.ap.write(rw)
             rw = []
             await self.seqr_port.get(rw)  # aka 'item_done'
-    #   endtask
 
-    
+
     async def drive_transaction(self, rw):
         if rw.read is False:
-            wdata = BinaryValue(value=rw.data,n_bits=self.n_bits)
+            #wdata = BinaryValue(value=rw.data,n_bits=self.n_bits)
             await RisingEdge(self.dut.clk)
             await Timer(0)
             self.dut.we <= 1
-            self.dut.data_in <= wdata
+            #print('DDWW data is ' + str(wdata))
+            #self.dut.data_in <= wdata
+            self.dut.data_in <= rw.data & self.bit_en
             self.dut.addr_in <= rw.addr
 
             for i in range(2):
@@ -137,29 +137,24 @@ class reg_driver(UVMComponent):
             self.dut.read <= 0x0
             uvm_info("REG_DRIVER READ", "Read value from DUT: " + str(rw.data) +
                 ' addr:' + str(rw.addr), UVM_LOW)
-            #rw.data = 0x6666
-    #endclass
+
+
 uvm_component_utils(reg_driver)
 
 
-#class reg_agent #(type DO=int) extends uvm_agent
 class reg_agent(UVMAgent):
-    #
-    #   reg_sequencer   sqr
-    #   reg_driver#(DO) drv
-    #   reg_monitor     mon
-    #
-    #   `uvm_component_param_utils_begin(reg_agent#(DO))
-    #      `uvm_field_object(sqr, UVM_ALL_ON)
-    #      `uvm_field_object(drv, UVM_ALL_ON)
-    #      `uvm_field_object(mon, UVM_ALL_ON)
-    #   `uvm_component_utils_end
-    #
+
+
     def __init__(self, name, parent=None):
         UVMAgent.__init__(self, name, parent)
         self.sqr = reg_sequencer("sqr", self)
         self.drv = reg_driver("drv", self)
         self.mon = reg_monitor("mon", self)
+        self.nbits = 32
+
+
+    def set_nbits(self, n):
+        self.nbits = n
 
     def build_phase(self, phase):
         UVMAgent.build_phase(self, phase)
@@ -170,12 +165,14 @@ class reg_agent(UVMAgent):
 
     def connect_phase(self, phase):
         self.drv.seqr_port.connect(self.sqr.seq_item_export)
-    #endclass
+        self.drv.set_nbits(self.nbits)
+
+
 uvm_component_utils(reg_agent)
 
 
 class reg2rw_adapter(UVMRegAdapter):
-    #
+
     def __init__(self, name="reg2rw_adapter"):
         UVMRegAdapter.__init__(self, name)
         self.supports_byte_enable = True
@@ -190,11 +187,9 @@ class reg2rw_adapter(UVMRegAdapter):
 
     def bus2reg(self, bus_item, rw):
         bus = bus_item
-        #if (!$cast(bus,bus_item)) begin
-        #    uvm_fatal("NOT_REG_TYPE","Provided bus_item is not of the correct type")
-        #    return
         rw.kind    = UVM_WRITE
-        if (bus.read):
+
+        if bus.read:
             rw.kind = UVM_READ
         rw.addr    = bus.addr
         rw.data    = bus.data
@@ -202,5 +197,5 @@ class reg2rw_adapter(UVMRegAdapter):
         rw.status  = UVM_IS_OK
         return rw
 
-    #endclass
+
 uvm_object_utils(reg2rw_adapter)
