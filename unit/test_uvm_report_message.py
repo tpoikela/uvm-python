2fc46b5d91e7829dd2d9c50116fed07cde1fa732
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-07-09 09:43:15 +0300

    Fixed failing unit test due to change of return type in clone()

--- unit/test_uvm_report_message.py
+++ unit/test_uvm_report_message.py
@@ -17,9 +17,14 @@ class TestUVMReportMessage(unittest.TestCase):
     def test_msg_copy(self):
         msg = UVMReportMessage.new_report_message()
         msg.set_message("My message is this")
+        msg.set_verbosity(111)
+        msg.set_filename('log.txt')
         msg_copy = msg.clone()
-        # Cannot be cloned
-        self.assertEqual(msg_copy, None)
+
+        self.assertEqual(msg_copy.get_name(), msg.get_name())
+        self.assertEqual(msg_copy.get_verbosity(), msg.get_verbosity())
+        self.assertEqual(msg_copy.get_filename(), msg.get_filename())
+
         msg_copy = UVMReportMessage()
         msg_copy.copy(msg)
         self.assertEqual(msg_copy.get_message(), msg.get_message())
