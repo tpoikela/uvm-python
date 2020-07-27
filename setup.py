661fa80f7d2cf04ce867ac66b1c6247e87886a5e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2020-07-27 23:11:27 +0300

    Bumped up supported cocotb commit to 79792d1b

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     setup_requires=["setuptools_scm",],
     python_requires=">=3.6",
     install_requires=[
-        "cocotb @ git+git://github.com/cocotb/cocotb@8de3aced#egg=cocotb",
+        "cocotb @ git+git://github.com/cocotb/cocotb@79792d1b#egg=cocotb",
         "cocotb-coverage>=1.0.0",
         "regex>=2019.11.1"
     ],
