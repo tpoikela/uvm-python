04c1f1f323f877f419a1ec8df5fdacf2d8940105
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2021-03-08 19:00:06 +0200

    Added -clean and -production for release script

--- devscripts/create_release.pl
+++ devscripts/create_release.pl
@@ -2,8 +2,23 @@
 
 use strict;
 
+use Getopt::Long;
+
+my %opt;
+GetOptions(
+    "production" => \$opt{production},
+    "clean" => \$opt{clean},
+);
+
+if (defined $opt{clean}) {
+    system("rm dist/*") if -d "./dist";
+}
+
 my $build_cmd = "python -m build";
 my $twine_cmd = "python -m twine upload --repository testpypi dist/*";
+if ($opt{production}) {
+    $twine_cmd = "python -m twine upload dist/*";
+}
 
 my $err = system($build_cmd);
 if ($err == 0) {
