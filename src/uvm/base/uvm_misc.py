dfab52f10d29a4be37711b10f9a1edecf8fa0c04
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2021-03-15 11:52:03 +0200

    Fixed the object types inside UVMStatusContainer

--- src/uvm/base/uvm_misc.py
+++ src/uvm/base/uvm_misc.py
@@ -31,7 +31,7 @@ UVM functionality, but such classes may be placed in :class:`UVMVoid`-typed
 containers along with other UVM objects.
 """
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Any
 
 import re
 from ..macros.uvm_message_defines import uvm_error, uvm_warning
@@ -278,10 +278,10 @@ class UVMStatusContainer:
 
         #  //These are the policy objects currently in use. The policy object gets set
         #  //when a function starts up. The macros use this.
-        self.comparer: Optional['UVMComparer'] = None  # UVMComparer
-        self.packer: Optional['UVMPacker'] = None  # UVMPacker
-        self.recorder: Optional['UVMRecorder'] = None  # UVMRecorder
-        self.printer: Optional['UVMPrinter'] = None  # UVMPrinter
+        self.comparer: Any  = None  # UVMComparer
+        self.packer: Any = None  # UVMPacker
+        self.recorder: Any  = None  # UVMRecorder
+        self.printer: Any = None  # UVMPrinter
 
     def do_field_check(self, field, obj):
         if UVM_ENABLE_FIELD_CHECKS:
